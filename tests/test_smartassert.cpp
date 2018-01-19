2b92d6b3ebec48d772961aab2096938cfdc544a8
Bruce <258214414@qq.com>
2018-01-19 10:37:52 +0800

    增加LOG宏

--- tests/test_smartassert.cpp
+++ tests/test_smartassert.cpp
@@ -1,13 +1,30 @@
 #include <iostream>
-#include "log.hpp"
+//#include "log.hpp"
 #include "SmartAssert.hpp"
 
 using namespace std;
 
+#if __cplusplus < 201103L
+//#error "should use C++11 implementation"
+#endif
+
+#define LOG(...) {\
+	fprintf(stderr, "%s: Line %d: Fun: %s\t", __FILE__, __LINE__, __func__);\
+	fprintf(stderr, __VA_ARGS__);\
+	fprintf(stderr, "\n");\
+}
+
+void test_assert();
+
 int main(int argc, char** argv)
 {
-    LOG_R_D("test SmartAssert");
+    LOG("test SmartAssert");
+	
+    return 0;
+}
+
+void test_assert()
+{
 	string str("this");
 	SMART_ASSERT(str.empty())(str);
-    return 0;
 }
